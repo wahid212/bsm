46f482698e734d6f08fcc11a503ac0ee26c34b82
wahid212 <newwolrds09@gmail.com>
2025-07-07 09:21:02 +0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Features from '../components/Features';
-import Footer from '../components/Footer';
-import Benefits from '../components/Benefits';
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Features from "../components/Features";
+import Footer from "../components/Footer";
+import Benefits from "../components/Benefits";
 
 function App() {
   return (
